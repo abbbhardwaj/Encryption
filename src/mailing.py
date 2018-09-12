15dae669c3f5d9f1c1c4cf86fd6fac2306f8db48
abhinav.bhardwaj05@gmail.com <abhinav.bhardwaj05@gmail.com>
2018-09-12 21:56:17 +0530

    added mailing file

---
+++ src/mailing.py
@@ -0,0 +1,21 @@
+import smtplib
+
+
+gmail_user = "abhinav.bhardwaj05@gmail.com"
+gmail_pwd = "amplitude"
+TO = 'razzabhu@gmail.com'
+SUBJECT = "Testing sending using gmail"
+TEXT = "Testing sending mail using gmail servers"
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.login(gmail_user, gmail_pwd)
+BODY = '\r\n'.join(['To: %s' % TO,
+        'From: %s' % gmail_user,
+        'Subject: %s' % SUBJECT,
+        '', TEXT])
+
+server.sendmail(gmail_user, [TO], BODY)
+print('email sent')
+
+
